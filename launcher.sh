e16299b042537ae4a2eb70b15ec8d75ea4faa602
Joker2770 <1214220480@qq.com>
2022-01-04 09:29:19 +0800

    docs: add license comment. :bookmark:

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,26 @@
+#
+#    This is a launch script.
+#    Copyright (C) 2021-2022 Jintao Yang <yjt950840@outlook.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License Version 2
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#    launcher.sh:
+#       launch script.
+#       launch GUI program by calling zenity on Linux.
+#
+
 #!/bin/sh
 
 xhost +
